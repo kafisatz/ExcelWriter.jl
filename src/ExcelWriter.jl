d79e1bdc0957b10b8d58ad4c5350a334324b93f2
kafisatz <github@diro.ch>
2020-08-03 14:30:40 +0200

    edits

--- src/ExcelWriter.jl
+++ src/ExcelWriter.jl
@@ -3,12 +3,6 @@ module ExcelWriter
 using DataFrames 
 using PyCall
 
-#=
-#originally written for
-  [a93c6f00] DataFrames v0.19.0 #7d5cb60 (https://github.com/JuliaData/DataFrames.jl.git)
-  [438e738f] PyCall v1.91.2 #0b2dc20 (https://github.com/JuliaPy/PyCall.jl.git)
-=#
-
 global const pyModnumpy = PyCall.PyNULL()
 global const pyModPandas = PyCall.PyNULL()
 global const pyModxlsxwriter = PyCall.PyNULL()
