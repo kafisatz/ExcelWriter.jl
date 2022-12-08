d5d640672ba8f68525ef849e8e7234074b19e00e
bernhard <bernhard.koenig@milliman.com>
2022-12-08 15:05:03 +0100

    suppress future warnings

--- src/ExcelWriter.jl
+++ src/ExcelWriter.jl
@@ -9,6 +9,12 @@ global const pyModxlsxwriter = PyCall.PyNULL()
 
 function __init__()
 	#the following lines may trigger the installation of the respective python packages	
+
+    #suppress FutureWarnings in Python
+    py"""import warnings
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    """
+
 	copy!(pyModnumpy, PyCall.pyimport_conda("numpy","numpy"))
 	copy!(pyModPandas, PyCall.pyimport_conda("pandas","pandas"))
 	copy!(pyModxlsxwriter, PyCall.pyimport_conda("xlsxwriter","xlsxwriter"))    
